93dc6a6eab05846fffbbb3af9677d24c3d1efb15
SemyonPyatkov <semen_p00@mail.ru>
2022-11-02 18:24:43 +0300

    add sql role insert

---
+++ .pack/sample-data.sql
@@ -0,0 +1,8 @@
+/*
+ Роли
+ */
+INSERT INTO public.roles (id, value, description) VALUES (1, 'admin', 'Администратор');
+INSERT INTO public.roles (id, value, description) VALUES (2, 'guide', 'Экскурсовод');
+INSERT INTO public.roles (id, value, description) VALUES (3, 'user', 'Пользователь');
+
+
